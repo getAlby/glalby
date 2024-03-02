eec94badbe6d2cde2d60e7d22333c60c3e7ce911
Michael Bumann <hello@michaelbumann.com>
2024-03-02 15:18:40 +0100

    Add dylib for macos

--- glalby/cgo.go
+++ glalby/cgo.go
@@ -3,5 +3,7 @@ package glalby
 /*
 #cgo LDFLAGS: -lglalby_bindings
 #cgo linux,amd64 LDFLAGS: -Wl,-rpath,${SRCDIR} -L${SRCDIR}
+#cgo darwin,arm64 LDFLAGS: -Wl,-rpath,${SRCDIR} -L${SRCDIR}
+#cgo darwin,amd64 LDFLAGS: -Wl,-rpath,${SRCDIR} -L${SRCDIR}
 */
 import "C"
